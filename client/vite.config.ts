70c5d688314bfa5c35a482c13cd2662599d6f825
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-06-12 00:49:39 +0530

    fixed minor issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import { BASE_PATH } from "./src/lib/constance";
-
 import path from "path";
 
 const getPath = (route: string) => path.resolve(__dirname, route);
+
+const BASE_PATH = "/todo-with-react-node-dynamodb/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +20,7 @@ export default defineConfig({
       "@/assets": getPath("./src/assets"),
       "@/lib": getPath("./src/lib"),
       "@/": getPath("./src"),
+      "~/": getPath("./"),
     },
   },
   plugins: [react()],
